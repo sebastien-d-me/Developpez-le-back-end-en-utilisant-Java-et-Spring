968c00a8dad18bd5c3b113e0db55e1aff94df329
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-01-02 23:23:50 +0100

    ✨ Ajout d'un message
    - Possibilité de laisser un message

--- back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/MessageController.java
+++ back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/MessageController.java
@@ -1,15 +1,32 @@
 package com.sebastiend.ChaTop.controllers;
 
+import java.io.IOException;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sebastiend.ChaTop.models.entities.MessageEntity;
+import com.sebastiend.ChaTop.models.entities.RentalEntity;
 import com.sebastiend.ChaTop.services.MessageService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Messages", description = "All routes for the messages.")
 public class MessageController {
     @Autowired
     private MessageService messageService;
 
+    @Operation(summary = "Create a message", description = "Create a message.", tags = { "Messages" })
+    @PostMapping("/api/messages")
+    public Map<String, String> createRental(@ModelAttribute MessageEntity message, @RequestParam("user_id") Integer user, @RequestParam("rental_id") Integer rental) throws IOException {
+        return messageService.saveMessage(message, user, rental);
+    }
 }
--- back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/services/MessageService.java
+++ back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/services/MessageService.java
@@ -1,6 +1,12 @@
 package com.sebastiend.ChaTop.services;
 
-import java.util.Optional;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,8 +22,16 @@ public class MessageService {
     @Autowired
     private MessageRepository messageRepository;
 
-    public MessageEntity saveMessage(MessageEntity message) {
-        MessageEntity savedMessage = messageRepository.save(message);
-        return savedMessage;
+    public Map<String, String> saveMessage(MessageEntity message, Integer user, Integer rental) throws IOException {
+        MessageEntity newMessage = new MessageEntity();
+        newMessage.setMessage(message.getMessage());
+        newMessage.setUserId(user);
+        newMessage.setRentalId(rental);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss");
+        LocalDateTime currentDate = LocalDateTime.now();
+        newMessage.setCreatedAt(dateFormatter.format(currentDate));
+        newMessage.setUpdatedAt(dateFormatter.format(currentDate));
+        messageRepository.save(newMessage);
+        return Map.of("message", "Message send with success");
     }
 }
