c523c525db50117aa145a96af0c0c55288e14360
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-01-16 23:18:36 +0100

    ♻️ Nettoyage du code
    - Suite du nettoyage du code

--- back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/RentalController.java
+++ back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/RentalController.java
@@ -1,30 +1,18 @@
 package com.sebastiend.ChaTop.controllers;
 
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.sebastiend.ChaTop.models.dto.Rentals.RentalCreateDTO;
-import com.sebastiend.ChaTop.models.dto.Rentals.RentalDTO;
-import com.sebastiend.ChaTop.models.dto.Rentals.RentalUpdateDTO;
+import com.sebastiend.ChaTop.models.dto.Rentals.*;
 import com.sebastiend.ChaTop.services.RentalService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.responses.*;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PutMapping;
+import java.io.IOException;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
@@ -33,13 +21,11 @@ public class RentalController {
     @Autowired
     private RentalService rentalService;
 
-    @Operation(summary = "Get all the rentals", description = "Get all the rentals.", tags = { "Rentals" })
-    @SecurityRequirement(name = "bearerAuth")
+
     @ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = 
-                "[\n"+
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "[\n"+
                     "{\n"+
                         "  \"id\": 1,\n"+
                         "  \"name\": \"name edited\",\n"+
@@ -63,60 +49,73 @@ public class RentalController {
                         "  \"updatedAt\": \"2025-01-13 20:37:22\"\n"+
                     "}\n"+
                 "]"
-            )
+            ),
+            mediaType = "application/json"
         )),
 		@ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = ""
+            ),
+            mediaType = "application/json"
         ))
 	})
     @GetMapping("/api/rentals")
+    @Operation(summary = "Get all the rentals", description = "Get all the rentals.", tags = { "Rentals" })
+    @SecurityRequirement(name = "bearerAuth")
     public List<RentalDTO> getRentals() {
         return rentalService.getRentals();
     }
 
-    @Operation(summary = "Get a specific rental", description = "Get a specific rental.", tags = { "Rentals" })
-    @SecurityRequirement(name = "bearerAuth")
-    @GetMapping("/api/rentals/{id}")
+
     @ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = 
-            "{\n"+
-                "  \"id\": 1,\n"+
-                "  \"name\": \"name edited\",\n"+
-                "  \"surface\": 50,\n"+
-                "  \"price\": 200,\n"+
-                "  \"pictureSrc\": \"1705ceab-7493-4-Screenshot_3.png\",\n"+
-                "  \"description\": \"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a lectus eleifend, varius massa ac, mollis tortor. Quisque ipsum nulla, faucibus ac metus a, eleifend efficitur augue. Integer vel pulvinar ipsum. Praesent mollis neque sed sagittis ultricies. Suspendisse congue ligula at justo molestie, eget cursus nulla tincidunt. Pellentesque elementum rhoncus arcu, viverra gravida turpis mattis in. Maecenas tempor elementum lorem vel ultricies. Nam tempus laoreet eros, et viverra libero tincidunt a. Nunc vel nisi vulputate, sodales massa eu, varius erat.\",\n"+
-                "  \"ownerId\": 1,\n"+
-                "  \"createdAt\": \"2025-01-13 20:36:58\",\n"+
-                "  \"updatedAt\": \"2025-01-13 20:37:37\"\n"+
-            "}"
-            )
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{\n"+
+                    "  \"id\": 1,\n"+
+                    "  \"name\": \"name edited\",\n"+
+                    "  \"surface\": 50,\n"+
+                    "  \"price\": 200,\n"+
+                    "  \"pictureSrc\": \"1705ceab-7493-4-Screenshot_3.png\",\n"+
+                    "  \"description\": \"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a lectus eleifend, varius massa ac, mollis tortor. Quisque ipsum nulla, faucibus ac metus a, eleifend efficitur augue. Integer vel pulvinar ipsum. Praesent mollis neque sed sagittis ultricies. Suspendisse congue ligula at justo molestie, eget cursus nulla tincidunt. Pellentesque elementum rhoncus arcu, viverra gravida turpis mattis in. Maecenas tempor elementum lorem vel ultricies. Nam tempus laoreet eros, et viverra libero tincidunt a. Nunc vel nisi vulputate, sodales massa eu, varius erat.\",\n"+
+                    "  \"ownerId\": 1,\n"+
+                    "  \"createdAt\": \"2025-01-13 20:36:58\",\n"+
+                    "  \"updatedAt\": \"2025-01-13 20:37:37\"\n"+
+                "}"
+            ),
+            mediaType = "application/json"  
         )),
 		@ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = ""
+            ),
+            mediaType = "application/json" 
         ))
 	})
+    @GetMapping("/api/rentals/{id}")
+    @Operation(summary = "Get a specific rental", description = "Get a specific rental.", tags = { "Rentals" })
+    @SecurityRequirement(name = "bearerAuth")
     public Optional<RentalDTO> getRental(@PathVariable Integer id) {
         return rentalService.getRental(id);
     }
 
-    @Operation(summary = "Create a rental", description = "Create a rental.", tags = { "Rentals" })
-    @SecurityRequirement(name = "bearerAuth")
-    @PostMapping(value = "/api/rentals", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+
     @ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "{\"message\": \"Rental created !\"}")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{\"message\": \"Rental created !\"}"
+            ),
+            mediaType = "application/json"
         )),
 		@ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = ""
+            ),
+            mediaType = "application/json"   
         ))
 	})
+    @Operation(summary = "Create a rental", description = "Create a rental.", tags = { "Rentals" })
+    @PostMapping(value = "/api/rentals", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @SecurityRequirement(name = "bearerAuth")
     public Map<String, String> createRental(
         @RequestParam("name") String name,
         @RequestParam("surface") String surface,
@@ -126,19 +125,25 @@ public class RentalController {
         return rentalService.saveRental(name, surface, price, picture, description, rental);
     }
 
-    @Operation(summary = "Edit a rental", description = "Edit a rental.", tags = { "Rentals" })
-    @SecurityRequirement(name = "bearerAuth")
+
+    
     @ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "{\"message\": \"Rental updated !\"}")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{\"message\": \"Rental updated !\"}"
+            ),
+            mediaType = "application/json"
         )),
 		@ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = ""
+            ),
+            mediaType = "application/json"   
         ))
 	})
+    @Operation(summary = "Edit a rental", description = "Edit a rental.", tags = { "Rentals" })
     @PutMapping(value="/api/rentals/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @SecurityRequirement(name = "bearerAuth")
     public Map<String, String> editRental(@PathVariable Integer id, @RequestParam(required = false) String name,
         @RequestParam(required = false) String surface,
         @RequestParam(required = false) String price,
--- back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/UserController.java
+++ back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/UserController.java
@@ -1,19 +1,15 @@
 package com.sebastiend.ChaTop.controllers;
 
 
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sebastiend.ChaTop.models.dto.Users.UserDTO;
 import com.sebastiend.ChaTop.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.*;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -22,29 +18,32 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Operation(summary = "Get a specific user", description = "Get a specific user.", tags = { "Users" })
-    @SecurityRequirement(name = "bearerAuth")
+
     @ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = 
-            "{\n"+
-                "  \"id\": 1,\n"+
-                "  \"name\": \"test TEST\",\n"+
-                "  \"email\": \"test@testa.com\",\n"+
-                "  \"createdAt\": \"2025-01-13 20:36:38\",\n"+
-                "  \"updatedAt\": \"2025-01-13 20:36:38\"\n"+
-            "}"
-            )
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{\n"+
+                    "  \"id\": 1,\n"+
+                    "  \"name\": \"test TEST\",\n"+
+                    "  \"email\": \"test@testa.com\",\n"+
+                    "  \"createdAt\": \"2025-01-13 20:36:38\",\n"+
+                    "  \"updatedAt\": \"2025-01-13 20:36:38\"\n"+
+                "}"
+            ),
+            mediaType = "application/json"  
         )),
 		@ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = ""
+            ),
+            mediaType = "application/json"
+            
         )),
 	})
     @GetMapping("/api/users/{id}")
+    @Operation(summary = "Get a specific user", description = "Get a specific user.", tags = { "Users" })
+    @SecurityRequirement(name = "bearerAuth")
     public Optional<UserDTO> getUser(@PathVariable Integer id) {
         return userService.getUser(id);
     }
-    
 }
