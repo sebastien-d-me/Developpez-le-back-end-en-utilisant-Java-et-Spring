9f1dda6ce4ead0d07da51afdfc613e2ffbed23f7
Sébastien D <81520399+sebastien-d-me@users.noreply.github.com>
2025-01-16 20:35:29 +0100

    ♻️ Nettoyage du code
    - Suite du nettoyage du code

--- back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/AuthenticationController.java
+++ back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/AuthenticationController.java
@@ -19,60 +19,72 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
     
-    @Operation(summary = "Create a new user account", description = "Create a new user account.", tags = { "Authentication" })
+
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "{\"token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c\"}")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{\"token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c\"}"
+            ),
+            mediaType = "application/json"
         )),
         @ApiResponse(responseCode = "400", description = "Bad request", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "{}")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{}"
+            ),
+            mediaType = "application/json"
         ))
     })
+    @Operation(summary = "Create a new user account", description = "Create a new user account.", tags = { "Authentication" })
     @PostMapping("/api/auth/register")
     public Map<String, String> registerUser(@RequestBody UserRegisterDTO userRegisterDTO) {
         return authenticationService.saveUser(userRegisterDTO);
     }
 
     
-    @Operation(summary = "Log in the user", description = "Log in the user.", tags = { "Authentication" })
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "{\"token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5ODc2NTQzMjEwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.3CGuVLhiJLvZfSW_2oB0QMkJNGtGRP6HF0ajCXtZmfI\"}")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{\"token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5ODc2NTQzMjEwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.3CGuVLhiJLvZfSW_2oB0QMkJNGtGRP6HF0ajCXtZmfI\"}"
+            ),
+            mediaType = "application/json"
         )),
         @ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "{\"message\": \"error\"}")
-        )),
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{\"message\": \"error\"}"
+            ),
+            mediaType = "application/json"
+        ))
     })
+    @Operation(summary = "Log in the user", description = "Log in the user.", tags = { "Authentication" })
     @PostMapping("/api/auth/login")
     public Map<String, String> loginUser(@RequestBody UserLoginDTO userLogin) throws AuthenticationException {
         return authenticationService.loginUser(userLogin);
 	}
 
-    @Operation(summary = "Get the info of the current logged user", description = "Get the info of the current logged user.", tags = { "Authentication" })
-    @SecurityRequirement(name = "bearerAuth")
+    
     @ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = 
-            "{\n"+
-                "  \"id\": 1,\n"+
-                "  \"name\": \"test TEST\",\n"+
-                "  \"email\": \"test@testa.com\",\n"+
-                "  \"createdAt\": \"2025-01-13 20:36:38\",\n"+
-                "  \"updatedAt\": \"2025-01-13 20:36:38\"\n"+
-            "}"
-            )
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{\n"+
+                    "  \"id\": 1,\n"+
+                    "  \"name\": \"test TEST\",\n"+
+                    "  \"email\": \"test@testa.com\",\n"+
+                    "  \"createdAt\": \"2025-01-13 20:36:38\",\n"+
+                    "  \"updatedAt\": \"2025-01-13 20:36:38\"\n"+
+                "}"
+            ),
+            mediaType = "application/json"
         )),
 		@ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "")
-        )),
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = ""
+            ),
+            mediaType = "application/json"
+        ))
 	})
     @GetMapping("/api/auth/me")
+    @Operation(summary = "Get the info of the current logged user", description = "Get the info of the current logged user.", tags = { "Authentication" })
+    @SecurityRequirement(name = "bearerAuth")
 	public UserDTO getMe() {
         return authenticationService.getMe();
 	}
--- back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/MessageController.java
+++ back-end/ChaTop/src/main/java/com/sebastiend/ChaTop/controllers/MessageController.java
@@ -1,20 +1,16 @@
 package com.sebastiend.ChaTop.controllers;
 
 
-import java.io.IOException;
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sebastiend.ChaTop.models.dto.Message.MessageDTO;
 import com.sebastiend.ChaTop.services.MessageService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.RequestBody;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.*;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -23,23 +19,30 @@ public class MessageController {
     @Autowired
     private MessageService messageService;
 
-    @Operation(summary = "Create a message", description = "Create a message.", tags = { "Messages" })
-    @SecurityRequirement(name = "bearerAuth")
+
     @ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "OK", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "{\"message\": \"Message send with success\"}")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{\"message\": \"Message send with success\"}"
+            ),
+            mediaType = "application/json"
         )),
         @ApiResponse(responseCode = "401", description = "Unauthorized", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = ""
+            ),
+            mediaType = "application/json" 
         )),
 		@ApiResponse(responseCode = "400", description = "Bad Request", content = @io.swagger.v3.oas.annotations.media.Content(
-            mediaType = "application/json",
-            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(value = "{}")
+            examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                value = "{}"
+            ),
+            mediaType = "application/json"
         ))
 	})
+    @Operation(summary = "Create a message", description = "Create a message.", tags = { "Messages" })
     @PostMapping("/api/messages")
+    @SecurityRequirement(name = "bearerAuth")
     public Map<String, String> createRental(@RequestBody MessageDTO message) throws IOException {
         return messageService.saveMessage(message);
     }
